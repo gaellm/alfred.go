e9a3220e4c4e5503da7cf0b679fbe1c88cfccf91
Gael Le Moellic <gael.lemoellic@orange.com>
2022-05-10 17:13:52 +0200

    Update helpers debug log

---
+++ internal/helper/helperReplacer.go
@@ -0,0 +1,43 @@
+/*
+ * Copyright The Alfred.go Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package helper
+
+import (
+	"errors"
+	"strings"
+)
+
+func HelperReplacement(data string, helpers []Helper) (string, error) {
+
+	failedHelpers := ""
+
+	for _, helper := range helpers {
+
+		if helper.Value == "" {
+			helper.Value = "unknown"
+			failedHelpers += helper.String + " "
+		}
+
+		data = strings.Replace(data, helper.String, helper.Value, -1)
+	}
+
+	if failedHelpers == "" {
+		return data, nil
+	}
+
+	return data, errors.New("helper hasn't capture values for these helpers string: " + failedHelpers)
+}
